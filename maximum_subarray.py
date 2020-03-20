0f850c7045577605a6b47355293f44e53d235294
Michael Le <admin@michaelworkspace.com>
2020-03-20 05:55:59 -0500

    annotating

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -1,9 +1,20 @@
-from typing import List
+"""
+Find the largest sum of a contiguous subarray (containing at least one number) within an array.
 
-# Brute force solution uses O(n^3) cubic time complexity, really bad.
+For example, given the array [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+the contiguous subarray [4, -1, 2, 1] has the largest sum of 6.
+"""
+
+
+from typing import List
 
 
 def maximum_array_brute_force(nums: List[int]) -> int:
+    """
+    Brute force solution uses double loops and within these nested loops our summation and slicing
+    of the array each takes O(n) time bringing the total time complexity to O(n^3) cubic time. This is bad.
+    """
+
     if len(nums) == 0:
         return 0
     max_sum = nums[0]
@@ -13,10 +24,12 @@ def maximum_array_brute_force(nums: List[int]) -> int:
     return max_sum
 
 
-# assert(maximum_array_brute_force([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6)
+assert(maximum_array_brute_force([-2, 1, -3, 4, -1, 2, 1, -5, 4]) == 6)
 
-# This approach uses a single loop with constant time operation so its time complexity is O(n).
+
 def maximum_array(nums: List[int]) -> int:
+    """ This approach uses a single loop with constant time operation so its time complexity is O(n). """
+
     if len(nums) == 0:
         return 0
     max_sum = nums[0]
