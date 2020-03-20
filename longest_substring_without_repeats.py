920709f200f24764b7bb36f966d53f3e5f42d286
Michael Le <admin@michaelworkspace.com>
2020-03-19 23:43:09 -0500

    Adding type annotations

---
+++ longest_substring_without_repeats.py
@@ -0,0 +1,30 @@
+"""
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+"abcabcbb" -> "abc" -> 3
+"bbbbb" -> "b" -> 1
+"pwwkew" -> "kew" -> 3
+"abcadcbf" -> "adcbf" -> 5
+
+Restrictions:
+Time complexity has to be O(n) linear time.
+"""
+
+
+def longest_substring_without_repeats(s: str) -> int:
+    seen: dict = {}
+    max_len = 0
+    start = 0
+    for end in range(len(s)):
+        if s[end] in seen:
+            start = max(start, seen[s[end]]+1)
+        seen[s[end]] = end
+        max_len = max(max_len, end-start+1)
+    return max_len
+
+
+assert longest_substring_without_repeats('abcabcbb') == 3
+assert longest_substring_without_repeats('bbbbb') == 1
+assert longest_substring_without_repeats('pwwkew') == 3
+assert longest_substring_without_repeats('abcadcbf') == 5
