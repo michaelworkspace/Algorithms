0f63875fa0437073ac9e34f7659dd83e10b5be7f
Michael Le <admin@michaelworkspace.com>
2020-11-17 18:18:35 -0500

    formatting

--- anagram.py
+++ anagram.py
@@ -1,14 +1,15 @@
-""" Anagram Algorithm Question
+""" is_anagram Algorithm Question
 
 
-Write a function that checks whether string 1 and string 2 are anagrams.
+Write a function that checks whether string 1 and string 2 are is_anagrams.
 """
 
 
-def anagram(s1: str, s2: str) -> bool:
+def is_anagram(s1: str, s2: str) -> bool:
     s1 = s1.replace(' ', '').lower()
     s2 = s2.replace(' ', '').lower()
 
     return sorted(s1) == sorted(s2)
 
-print(anagram('d oG', 'goD'))
+print(is_anagram('d oG', 'goD'))
+
